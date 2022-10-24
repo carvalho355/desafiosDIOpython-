2b26973f0c0fa5150d08cc0cd7d2f436f6152e01
carvalho355 <carlos.ce355@gmail.com>
2022-10-23 22:14:03 -0300

    git commit -m 'Adicionado vírgula na linha 17'

--- image-processing-package/setup.py
+++ image-processing-package/setup.py
@@ -14,7 +14,7 @@ setup(
     description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/carvalho355/desafiosDIOpython-.git"
+    url="https://github.com/carvalho355/desafiosDIOpython-.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
